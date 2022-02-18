ad53b41b417cd1e56f7b75a9b61ba8a8d2e9ad63
ignatenkoas <ignatenkoasnvr@gmail.com>
2022-02-19 02:24:31 +0300

    finish

--- Main.cpp
+++ Main.cpp
@@ -26,92 +26,155 @@ int evkd(int a, int b) {
 class Fraction
 {
 private:
-	int m_integer = 0;
 	int m_numerator;
 	int m_denominator;
 public:
 	Fraction()
 	{  
-		m_integer = 0;
 		m_numerator = 0;
 		m_denominator = 1;
 	}
-	Fraction(int numerator, int denominater = 1)
-	{
-		assert(denominater != 0);
-		Fraction(int numerator, int denominater = 1)
-		{
-			assert(denominater != 0);
-			if  
-			m_numerator = numerator;
-			m_denominator = denominater;
-
-		}
+	Fraction(int numerator, int denominater = 1){
+		assert(denominater != 0); 
 		m_numerator = numerator;
 		m_denominator = denominater;
 	}
-	Fraction(int numerator, int denominater = 1)
-	{
-		assert(denominater != 0);
-		m_numerator = numerator;
-		m_denominator = denominater;
-	}
-
 	int getNumerator() const {return m_numerator;}
 	int getDenominater() const {return m_denominator;}
-	double getValue()
+	float getFloat()
 	{
-		return static_cast <double> (m_numerator) / m_denominator; //  подсмотрел оператор явного преобразования фунд.типов данных
+		return static_cast <float> (m_numerator) / m_denominator; //  подсмотрел оператор явного преобразования фунд.типов данных
 	}
 	Fraction& print() 
 	{
-		cout << m_numerator << "/" << m_denominator << endl;
+		cout << m_numerator << "/" << m_denominator;
 		return *this;
 	}
-	//Fraction& add(Fraction& drob1, Fraction& drob2)
-	//{
-
-	//	return *this;
-	//}
-
-	void reduction()
-	{
+	void reduction(int &m_numerator, int &m_denominator){
 		int numberEvkd = evkd(m_numerator, m_denominator);
 		m_numerator = m_numerator / numberEvkd;
 		m_denominator = m_denominator / numberEvkd;
 	}
-	Fraction& add(Fraction& drob)
+	Fraction operator + (Fraction& that)
+	{
+		int tmp = this->m_denominator;
+		int denominator = this->m_denominator * that.m_denominator;
+		int numerator = this->m_numerator * that.m_denominator + that.m_numerator * tmp;
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
+	}
+	Fraction operator -(Fraction& that)
 	{
 		int tmp = m_denominator;
-		m_denominator = m_denominator * drob.m_denominator;
-		m_numerator = m_numerator * drob.m_denominator + drob.m_numerator * tmp;
-		reduction();
-		return *this;
+		int denominator = m_denominator * that.m_denominator;
+		int numerator = m_numerator * that.m_denominator - that.m_numerator * tmp;
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
 	}
-	Fraction& sub(Fraction& drob)
+	Fraction operator *(Fraction& that)
 	{
-		int tmp = m_denominator;
-		m_denominator = m_denominator * drob.m_denominator;
-		m_numerator = drob.m_numerator * tmp - m_numerator * drob.m_denominator;
-		reduction();
+		int denominator = m_denominator * that.m_denominator;
+		int numerator = that.m_numerator * m_numerator;
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
+	}
+	Fraction operator /(Fraction& that)
+	{
+		int denominator = m_denominator * that.m_numerator;
+		int numerator = that.m_denominator * m_numerator;
+		reduction(numerator, denominator);
+		return Fraction(numerator, denominator);
+	}
+	Fraction operator ++ (){
+		m_numerator += m_denominator;
 		return *this;
 	}
-
+	Fraction operator -- () {
+		m_numerator -= m_denominator;
+		return *this;
+	}	
+	Fraction operator ++ (int) {
+		Fraction copy(*this);
+		m_numerator += m_denominator;
+		return copy;
+	}
+	Fraction operator -- (int) {
+		Fraction copy(*this);
+		m_numerator -= m_denominator;
+		return copy;
+	}
+	bool operator <(Fraction& that)
+	{
+		if (this->getFloat() - that.getFloat() < 0)
+			return true;
+		else
+			return false;
+	}
+	bool operator >(Fraction& that)
+	{
+		if (this->getFloat() - that.getFloat() > 0)
+			return true;
+		else
+			return false;
+	}
+	static Fraction intToFraction(int tmp){
+		return Fraction(tmp, 1);
+	}
 };
 
+int main(){
+	Fraction first(1, 2);
+	Fraction second(3, 5);
+	//first.print();
+	//second.print();
 
-int main()
-{
-	Fraction drob2(7, 8);
-	Fraction drob3(5, 9);
-	drob2.print();
-	drob3.print();
-	drob2.reduction();
-	drob3.reduction();
-	drob2.print();
-	drob3.print();
+	Fraction third = first + second;
+	first.print();
+	cout << " + ";
+	second.print();
+	cout << " = ";
+	third.print();
+	cout << endl;
 
-	drob3.sub(drob2);
-	drob3.print();
+	Fraction fourth = second * third;
+	second.print();
+	cout << " * ";
+	third.print();
+	cout << " = ";
+	fourth.print();
+	cout << endl;
+
+	Fraction fifth = fourth / second;
+	fourth.print();
+	cout << " / ";
+	second.print();
+	cout << " = ";
+	fifth.print();
+	cout << endl;
+
+	Fraction sixth = fifth - first;
+	fifth.print();
+	cout << " - ";
+	first.print();
+	cout << " = ";
+	sixth.print();
+	cout << endl;
+
+	if (fifth < sixth)
+		cout << "fifth < sixth" << endl;
+	else if (fifth > sixth)
+		cout << "fifth > sixth" << endl;
+	else
+		cout << "fifth = sixth" << endl;
+
+	cout << second.getFloat()  << endl;
+
+	Fraction intToFraction(5);
+	++first.print();
+	cout << endl;
+	first--.print();
+	cout << endl;
+	first.print();
+
 	return 0;
-}
+} 
